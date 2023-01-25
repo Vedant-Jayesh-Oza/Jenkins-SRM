27e99adb832eb656a75cac3c3129f0ab0f42f7a6
Vedant Jayesh Oza <75005433+Vedant-Jayesh-Oza@users.noreply.github.com>
2023-01-25 10:23:37 +0530

    Delete jenkinsrm.java

--- jenkinsrm.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "Hello, World!";
-        int length = str.length();
-        System.out.println("Length of the string: " + length);
-    }
-}
-

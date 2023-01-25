fca1de436414ea92cecdee92db391faf4fa5224b
Vedant Jayesh Oza <75005433+Vedant-Jayesh-Oza@users.noreply.github.com>
2023-01-25 09:30:32 +0530

    Add files via upload

---
+++ jenkinsrm.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int length = str.length();
+        System.out.println("Length of the string: " + length);
+    }
+}
+
